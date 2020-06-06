308f9275faed401d079393e117731a72b64ff07c
harvey magana <heyharv2012@gmail.com>
2020-06-06 09:13:14 -0700

    Completed Task 3, added the getYears function.

--- index.js
+++ index.js
@@ -38,13 +38,17 @@ console.log(getFinals(fifaData))
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
-
+function getYears(callback) {
+    let years = [];
+    return callback.reduce((year, state) => { 
+        if(state.Stage === 'Final') {
+            years.push(state.Year)
+        }
+        return years;
+    }, []);
 };
 
-getYears();
+console.log(getYears(fifaData));
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
